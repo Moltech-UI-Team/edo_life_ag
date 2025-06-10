42f3d5908a1a31553bf8d9c4235b287ef5ea5530
Praful Desai <praful.d@mol-tech.com>
2025-06-10 10:31:27 +0530

    Link added for all calendly !!

--- src/components/ui/button/Button.js
+++ src/components/ui/button/Button.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import "./Button.scss"
+import React from 'react';
+import './Button.scss';
 
-const Button = ({label}) => {
+const Button = ({ label }) => {
   return (
-    <button className="button">{label}</button>
-  )
-}
+    <a 
+      href="https://calendly.com/hermann_life_institute/strategiesitzung-clone" 
+      className="button" 
+      target="_blank" 
+      rel="noopener noreferrer"
+    >
+      {label}
+    </a>
+  );
+};
 
-export default Button
+export default Button;
